a2a759f3d851b275bde3e53581ca46f8169c682a
antte <andreas.fliesberg@gmail.com>
2010-03-10 10:23:23 +0100

    databas med people och två test personer

---
+++ sql/dbdata.sql
@@ -0,0 +1,22 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 10, 2010 at 09:22 AM
+-- Server version: 5.1.36
+-- PHP Version: 5.3.0
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `hope`
+--
+
+--
+-- Dumping data for table `people`
+--
+
+INSERT INTO `people` (`id`, `name`, `email`) VALUES
+(1, 'Nisse', 'nisse@sbf.se'),
+(2, 'Hasse', 'hasse@sbf.se');
