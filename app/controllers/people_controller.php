4ba6cfcff87bf2548c38a7e279d0612d1b76b74f
Olle Svensson <bolliolle@gmail.com>
2010-03-10 14:09:11 +0100

    olle

--- app/controllers/people_controller.php
+++ app/controllers/people_controller.php
@@ -9,4 +9,7 @@
 		function index() {
 			 $this->set('people', $this->Person->find('all'));
 		}
+		function view($id) {
+			$this->set('person', $this->Person->find('all',array( 'conditions' => array('id' => $id))));
+		} 
 	}
